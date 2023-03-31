4382eb4c2d290f2f7ce2a9a7270b973f630c16f7
laxmikantborale <121218711+laxmikantborale@users.noreply.github.com>
2023-03-31 16:06:02 +0530

    Add files via upload

---
+++ sapient_task/app.js
@@ -0,0 +1,9 @@
+// function validate(e) {
+//     e.preventDefault()
+//     let username = document.getElementById('text').value;
+
+//     if(username == "") {
+//         document.getElementById('message').innerHTML= "enter username";
+//         return false;
+//     }
+// }
